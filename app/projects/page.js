0934e458a1887dad5c9e32ad1b17ffa9379fb7ae
juanserodriguez29 <juanserodriguez29@hotmail.com>
2024-01-26 17:19:10 -0500

    Update projects and about page responsive

--- app/projects/page.js
+++ app/projects/page.js
@@ -25,12 +25,13 @@ const prevIndex = ()=>{
       <section className={classes.section}>
         <div className={classes.image}><ProjectImage initialIndex={initialIndex}/></div>
         <div className={classes.text}><ProjectText initialIndex={initialIndex}/></div>
+        <div className={classes.buttons}>
+          <a className={classes.firstButton} onClick={()=>prevIndex()}><svg className={classes.rightArrow} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#f9572a" stroke="#f9572a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"/></svg></a>
+          <a className={classes.secondButton} onClick={()=>posIndex()}><svg className={classes.leftArrow} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#f9572a" stroke="#f9572a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/></svg></a>
+        </div>
       </section>
 
-      <div className={classes.buttons}>
-        <a className={classes.firstButton} onClick={()=>prevIndex()}><svg className={classes.rightArrow} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#f9572a" stroke="#f9572a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 .755l-14.374 11.245 14.374 11.219-.619.781-15.381-12 15.391-12 .609.755z"/></svg></a>
-        <a className={classes.secondButton} onClick={()=>posIndex()}><svg className={classes.leftArrow} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#f9572a" stroke="#f9572a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 .755l14.374 11.245-14.374 11.219.619.781 15.381-12-15.391-12-.609.755z"/></svg></a>
-      </div>
+      
     </>
   );
 }
